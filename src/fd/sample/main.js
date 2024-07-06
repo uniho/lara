725e1855e69f3d7cc482214e080105d1b3f756ce
uniho <uniho@users.noreply.github.com>
2024-07-06 17:54:29 +0900

    remove MUI

--- src/fd/sample/main.js
+++ src/fd/sample/main.js
@@ -1,34 +1,26 @@
+import React from "https://esm.sh/react@canary?dev";
+import ReactDOM from "https://esm.sh/react-dom@canary?dev";
+import ReactDOMClient from "https://esm.sh/react-dom@canary/client?dev";
+// import ReactDOMServer from "https://esm.sh/react-dom@canary/server.browser?dev";
+import htm from "https://esm.sh/htm";
+import * as emotion from "https://esm.sh/@emotion/css@11";
+
+window.React = React;
+window.ReactDOM = ReactDOM;
+window.ReactDOMClient = ReactDOMClient;
+// window.ReactDOMServer = ReactDOMServer;
+window.htm = htm;
+window.html = htm.bind(React.createElement)
+window.emotion = emotion;
+
+window.Fragment = React.Fragment;
+window.Suspense = React.Suspense;
 
 //
 export const main = async props => {
-  const page = await import(`~/?js_route=sample.${props.page ?? 1}`);
+  const page = await import(`jsx/sample/${props.page ?? 1}.jsx`);
   const newProps = Object.assign(props, page.main ? await page.main(props) : {});
   const Page = page.default;
-  const root = ReactDOM.createRoot(document.getElementById("app"));
+  const root = ReactDOMClient.createRoot(document.getElementById("app"));
   root.render(React.createElement(Page, newProps));
 }
-
-// htm is JSX-like syntax in plain JavaScript - no transpiler necessary.
-// https://github.com/developit/htm
-window.html = htm.bind(React.createElement);
-
-// Material is a framework for sleek, responsive web interfaces.
-// https://mui.com/
-window.MUI = MaterialUI;
-window.styled = MaterialUI.styled;
-window.css = MaterialUI.css;
-window.keyframes = MaterialUI.keyframes;
-
-// Define useful Sx Components
-window.Sx = {
-  div: MaterialUI.styled('div')({}),
-  span: MaterialUI.styled('span')({}),
-  flex: MaterialUI.styled('div')({display:'flex'}),
-  flexCol: MaterialUI.styled('div')({display:'flex', flexDirection:'column'}),
-};
-
-// clsx is a utility for constructing className strings conditionally.
-// https://github.com/lukeed/clsx
-//
-//   You can use `clsx` instead of `cx` for this purpose.
-//   https://emotion.sh/docs/@emotion/css#cx
