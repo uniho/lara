faa60ba6935b9cb8479434c19830f90aefb2191c
uniho <uniho@users.noreply.github.com>
2024-07-14 08:53:31 +0900

    move async dir to cli/async dir

--- src/bd/modules/Utils/AsyncCLI.php
+++ src/bd/modules/Utils/AsyncCLI.php
@@ -7,7 +7,7 @@ final class AsyncCLI
   public static function runArtisan($params = '')
   {
     $php = \HQ::getenv('CCC::PHP_CLI');
-    $cmd = __DIR__ . '/../../async/cli/artisan.php ' . $params;
+    $cmd = \HQ::getenv('CCC::CLI_PATH') . '/async/artisan.php ' . $params;
     exec("$php $cmd > /dev/null &");
   }
 }
