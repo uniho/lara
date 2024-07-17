883e1d1dc380e92e2a92c06677dd2d5e06181356
uniho <uniho@users.noreply.github.com>
2024-07-17 13:05:02 +0900

    fix a bug in CachedConfig

--- src/bd/modules/CachedConfig.php
+++ src/bd/modules/CachedConfig.php
@@ -2,40 +2,40 @@
 
 final class CachedConfig
 {
-  protected static $KEY; 
+  protected static $KEY = 'CACHED_CONFIG'; 
 
-  public static function __init()
+  public static function __init($key = false)
   {
-    self::$KEY = 'CACHED_CONFIG'; 
+    self::$KEY = $key ?: self::$KEY; 
   }
 
   public static function get($key)
   {
-    $config = \Unsta\KVEFile::fetch(self::$KEY);
+    $config = \HQ::cache()->get(self::$KEY);
     if (!isset($config[$key])) return null;
     return $config[$key];
   }
 
   public static function set($key, $val)
   {
-    $config = \Unsta\KVEFile::fetch(self::$KEY);
+    $config = \HQ::cache()->get(self::$KEY);
     if (!$config) $config = [];
     $config[$key] = $val;
-    return \Unsta\KVEFile::store(self::$KEY, $config);
+    return \HQ::cache()->put(self::$KEY, $config);
   }
 
   public static function exists($key)
   {
-    $config = \Unsta\KVEFile::fetch(self::$KEY);
+    $config = \HQ::cache()->get(self::$KEY);
     return isset($config[$key]);
   }
 
   public static function delete($key)
   {
-    $config = \Unsta\KVEFile::fetch(self::$KEY);
+    $config = \HQ::cache()->get(self::$KEY);
     if (!isset($config[$key])) return true;
     unset($config[$key]);
-    return \Unsta\KVEFile::store(self::$KEY, $config);
+    return \HQ::cache()->put(self::$KEY, $config);
   }
 }
 
