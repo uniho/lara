1d6c0926333285961e22ab53811e62e8fd173e8e
uniho <uniho@users.noreply.github.com>
2024-08-19 21:12:04 +0900

    improve set config

--- src/bd/modules/Providers/AppServiceProvider.php
+++ src/bd/modules/Providers/AppServiceProvider.php
@@ -87,36 +87,88 @@ class AppServiceProvider extends ServiceProvider
     });
 
     //
-    \View::addLocation(\HQ::getenv('CCC::VIEWS_PATH'));
-
-    //
     if (is_file(\HQ::getenv('CCC::STORAGE_FILE_VIEW_CACHE_CLEAR'))) {
       @unlink(\HQ::getenv('CCC::STORAGE_FILE_VIEW_CACHE_CLEAR'));
       \File::deleteDirectory(app()['config']['view.compiled'], true);
     }
 
     // set config values
-    config(\Arr::dot(require(\HQ::getConfigFile())));
-    date_default_timezone_set(config('app.timezone', 'UTC'));
+    $configFile = app()->getCachedConfigPath();
+    $configFileCustom = \HQ::getConfigFile();
+    if (!is_file($configFile) || filemtime($configFile) !== filemtime($configFileCustom)) {
+      
+      $configCustom = require(\HQ::getConfigFile());
+      config(\Arr::dot($configCustom));
+      if (isset($configCustom['app']['timezone'])) {
+        date_default_timezone_set($configCustom['app']['timezone']);
+      }
+  
+      if (!is_file(\HQ::getenv('CCC::FILE_APP_KEY'))) {
+        @mkdir(dirname(\HQ::getenv('CCC::FILE_APP_KEY')), 0777, true);
+        $key = base64_encode(\Illuminate\Support\Str::random(32));
+        @file_put_contents(\HQ::getenv('CCC::FILE_APP_KEY'), "base64:$key");
+      }
 
-    if (!is_file(\HQ::getenv('CCC::FILE_APP_KEY'))) {
-      @mkdir(dirname(\HQ::getenv('CCC::FILE_APP_KEY')), 0777, true);
-      $key = base64_encode(\Illuminate\Support\Str::random(32));
-      @file_put_contents(\HQ::getenv('CCC::FILE_APP_KEY'), "base64:$key");
+      \View::addLocation(\HQ::getenv('CCC::VIEWS_PATH'));
+
+      config([
+        'app.key' => @file_get_contents(\HQ::getenv('CCC::FILE_APP_KEY')),
+        'app.debug' => \HQ::getDebugMode() || \HQ::getenv('debug'),
+        'filesystems.disks.local.root' => \HQ::getenv('CCC::STORAGE_LOCAL_PRIVATE_FILES_PATH'),
+        'filesystems.disks.public.root' => \HQ::getenv('CCC::STORAGE_LOCAL_PUBLIC_FILES_PATH'),
+        'view.cache' => \HQ::getViewCacheMode(),
+        // 'view.paths' => [
+        //   \HQ::getenv('CCC::VIEWS_PATH'),
+        //   resource_path('views'),
+        // ],
+        'session.cookie' => \HQ::getAppSlug().'_session',
+        'session.path' => \HQ::getCookiePath(),
+        'debugbar.storage.open' => true,
+        'debugbar.collectors.log' => true,
+        'debugbar.inject' => \HQ::getDebugbarShowAlways() || \HQ::getenv('debug'),
+      ]);
+  
+      $config = var_export(config()->all(), true);
+      \File::put($configFile, "<?php return array_replace_recursive(".$config.", require(__DIR__.'/config1.php'), [
+        'app' => [
+          'key' => @file_get_contents(\HQ::getenv('CCC::FILE_APP_KEY')),
+          'debug' => \HQ::getDebugMode() || \HQ::getenv('debug'),
+        ],
+        'filesystems' => [
+          'disks' => [
+            'local' => [
+              'root' => \HQ::getenv('CCC::STORAGE_LOCAL_PRIVATE_FILES_PATH'),
+            ],
+            'public' => [
+              'root' => \HQ::getenv('CCC::STORAGE_LOCAL_PUBLIC_FILES_PATH'),
+            ],
+          ],
+        ],
+        'view' => [
+          'cache' => \HQ::getViewCacheMode(),
+          'paths' => [
+            \HQ::getenv('CCC::VIEWS_PATH'),
+            resource_path('views'),
+          ],
+        ],
+        'session' => [
+          'cookie' => \HQ::getAppSlug().'_session',
+          'path' => \HQ::getCookiePath(),
+        ],
+        'debugbar' => [
+          'storage' => [
+            'open' => true,
+          ],
+          'collectors' => [
+            'logs' => true,
+          ],
+          'inject' => \HQ::getDebugbarShowAlways() || \HQ::getenv('debug'),
+        ],  
+      ]);");
+
+      \touch($configFile, filemtime($configFileCustom));          
+      \File::copy($configFileCustom, \File::dirname($configFile).'/config1.php');
     }
-    
-    config([
-      'app.key' => @file_get_contents(\HQ::getenv('CCC::FILE_APP_KEY')),
-      'app.debug' => \HQ::getDebugMode() || \HQ::getenv('debug'),
-      'filesystems.disks.local.root' => \HQ::getenv('CCC::STORAGE_LOCAL_PRIVATE_FILES_PATH'),
-      'filesystems.disks.public.root' => \HQ::getenv('CCC::STORAGE_LOCAL_PUBLIC_FILES_PATH'),
-      'view.cache' => \HQ::getViewCacheMode(),
-      'session.cookie' => \HQ::getAppSlug().'_session',
-      'session.path' => \HQ::getCookiePath(),
-      'debugbar.storage.open' => true,
-      'debugbar.collectors.log' => true,
-      'debugbar.inject' => \HQ::getDebugbarShowAlways() || \HQ::getenv('debug'),
-    ]);
 
     //
     \HQ::onBoot();
