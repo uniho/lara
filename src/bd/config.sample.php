d8a823ff2d84b0991fa9dd083b602e859aae2c69
uniho <uniho@users.noreply.github.com>
2024-12-19 08:42:36 +0900

    bump Laravel to v11.36.1

--- src/bd/config.sample.php
+++ src/bd/config.sample.php
@@ -49,7 +49,6 @@ return [
       'smtp' => [
         'host' =>  'your smtp domain',
         'port' => 587,
-        'encryption' => 'tls',
         'username' => 'your smtp user',
         'password' => 'your smtp pass',
       ],
