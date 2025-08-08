480cbda9a870669d72c5e380586c20580b482b28
uniho <uniho@users.noreply.github.com>
2025-08-08 10:34:13 +0900

    chore

--- src/.bd/on.php
+++ src/.bd/on.php
@@ -18,7 +18,7 @@ class On
     \HQ::setenv('CCC::APP_NAME', 'Test App!');
 
     \HQ::setAppSlug('lara');
-    // \HQ::setCookiePath('/lara'); // サブディレクトリに導入する場合に指定
+    \HQ::setCookiePath('/lara'); // サブディレクトリに導入する場合に指定。ルートの場合は '/'
 
     \HQ::setDebugMode(true);
     \HQ::setViewCacheMode(!\HQ::getDebugMode());
