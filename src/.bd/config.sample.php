c359976d518ae8a4e783e951ec50358b5fd4418f
uniho <uniho@users.noreply.github.com>
2025-10-07 04:31:07 +0900

    chore

--- src/.bd/config.sample.php
+++ src/.bd/config.sample.php
@@ -2,41 +2,45 @@
 
 return [
 
-  'app' => [
-    
-    /*
-    |--------------------------------------------------------------------------
-    | Application Timezone
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify the default timezone for your application, which
-    | will be used by the PHP date and date-time functions. We have gone
-    | ahead and set this to a sensible default for you out of the box.
-    |
-    */
+    'app' => [
+        
+        /*
+        |--------------------------------------------------------------------------
+        | Application Timezone
+        |--------------------------------------------------------------------------
+        |
+        | Here you may specify the default timezone for your application, which
+        | will be used by the PHP date and date-time functions. We have gone
+        | ahead and set this to a sensible default for you out of the box.
+        |
+        */
 
-    'timezone' => 'UTC',
-  ],
-
-  'mail' => defined('SSS::email') ? (\SSS::email ?? null) : null,
-
-  'logging' => [
-    'default' => 'daily',
-    'channels' => [
-      'daily' => [
-        'driver' => 'daily',
-        'path' => \HQ::getenv('CCC::KEEP_PATH').'/logs/laravel.log',
-        'level' => 'debug',
-        'days' => 90,
-        'replace_placeholders' => true,
-      ],
+        'timezone' => 'UTC',
     ],
-  ],
 
-  'log-viewer' => [
-    'include_files' => [
-      \HQ::getenv('CCC::KEEP_PATH').'/logs/*.log' => 'keep',
+    'mail' => defined('SSS::email') ? (\SSS::email ?? null) : null,
+
+    'logging' => [
+        'default' => 'daily',
+        'channels' => [
+            'daily' => [
+                'driver' => 'daily',
+                'path' => \HQ::getenv('CCC::KEEP_PATH').'/logs/laravel.log',
+                'level' => 'debug',
+                'days' => 90,
+                'replace_placeholders' => true,
+            ],
+        ],
     ],
-  ],
+
+    'log-viewer' => [
+        'route_path' => 'admin/logs',
+        'include_files' => [
+            \HQ::getenv('CCC::KEEP_PATH').'/logs/*.log' => 'keep',
+        ],
+        'defaults' => [
+            'per_page' => 100,
+        ],
+    ],
 
 ];
