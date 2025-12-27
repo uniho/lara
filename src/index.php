f0b884ef354ba306be40a4e43a39508f6b9daf3a
uniho <uniho@users.noreply.github.com>
2025-12-27 10:44:53 +0900

    add scoped_require helper function

--- src/index.php
+++ src/index.php
@@ -1,20 +1,20 @@
 <?php
-// v12.250621
+// v12.251227
 
 define('LARAVEL_START', microtime(true));
 
 // Check If The Application Is Under Maintenance
 if (!is_dir(__DIR__.'/.bd/laravel/storage/framework')) {
-  header ('HTTP/1.0 503 Service Temporarily Unavailable');
-  exit();
+    header ('HTTP/1.0 503 Service Temporarily Unavailable');
+    exit();
 }
 if (is_file(__DIR__.'/.bd/laravel/storage/framework/down')) {
-  $query = [];
-  parse_str($_SERVER['QUERY_STRING'], $query);
-  if (isset($query['secret4down'])) {
-    $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-    $_SERVER['REQUEST_URI'] = $path . $query['secret4down'];
-  }
+    $query = [];
+    parse_str($_SERVER['QUERY_STRING'], $query);
+    if (isset($query['secret4down'])) {
+        $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        $_SERVER['REQUEST_URI'] = $path . $query['secret4down'];
+    }
 }
 
 // .bd/modules ディレクトリ内のモジュールをオートロードする
@@ -22,6 +22,15 @@ require_once '.bd/modules/autoload.php';
 
 // .bd/laravel/vendor ディレクトリ内のモジュールをオートロードする
 require_once '.bd/laravel/vendor/autoload.php';
+
+// A helper function
+function scoped_require(string $path, array $vars = [])
+{
+    extract($vars);
+    return require $path;
+}
+
+// Starting Laravel...
 
 \HQ::onStart();
 
